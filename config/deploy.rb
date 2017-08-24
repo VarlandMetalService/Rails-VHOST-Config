9d6fdd6315d06117b4fe747f546bf0b79548d492
Toby Varland <toby@tobyvarland.com>
2017-08-24 16:35:46 -0400

    Capistrano Configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,13 @@
 # config valid only for current version of Capistrano
 lock "3.9.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "Rails-VHOST-Config"
+set :repo_url, "https://github.com/VarlandMetalService/Rails-VHOST-Config.git"
+
+set :deploy_to, '/home/vcms/Rails-VHOST-Config'
+
+#append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
